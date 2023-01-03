09e198f8e57149cc4d05dc893ac4051828e0cc8d
Nilesh Patil <abhishekchidrawar@gmail.com>
2023-01-03 12:53:44 +0530

    Project added

---
+++ VuejsProjectClient/src/App.vue
@@ -0,0 +1,34 @@
+
+
+<script>
+    import HelloWorld from './components/HelloWorld.vue'
+    import Navbar from './components/Navbar.vue'
+    import ButtonCounter from './components/ButtonCounter.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            ButtonCounter,
+            //Navbar,
+            HelloWorld
+        }
+    }
+</script>
+
+<template>
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <h2>Here is Child component</h2>
+    <ButtonCounter />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 50px;
+    }
+</style>
---
+++ VuejsProjectClient/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <h1>To-Do List</h1>
+        <ul>
+            <li>
+                <navbar></navbar>
+            </li>
+        </ul>
+    </div>
+
+    <div>
+        <input type="checkbox" id="todo-item" />
+        <label for="todo-item">My Todo Item</label>
+    </div>
+</template>
+<script>
+    export default {};
+</script>
